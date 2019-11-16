8328a6d8080cdcf70c452695b73081ce38fd3d68
PARJINDER S PANNU <parjinderpannu@gmail.com>
2019-11-16 12:18:02 -0800

    /books post return request

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -20,6 +20,30 @@ books = [
 @app.route('/books')
 def get_books():
     return jsonify({'books': books})
+
+
+# POST /books
+# {
+#  'name': 'F',
+#  'price': 6.99,
+#  'isbn': 0123456789
+# }
+
+@app.route('/books', methods=['POST'])
+def add_book():
+    return jsonify(request.get_json())
+
+
+@app.route('/books/<int:isbn>')
+def get_book_by_isbn(isbn):
+    return_value = {}
+    for book in books:
+        if book["isbn"] == isbn:
+            return_value = {
+                'name': book["name"],
+                'price': book["price"]
+            }
+    return jsonify(return_value)
 
 
 app.run(port=5000)
