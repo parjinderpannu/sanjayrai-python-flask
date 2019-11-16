7c6db5a8bed7f92bce25f2f18c07f94f3af3b799
PARJINDER S PANNU <parjinderpannu@gmail.com>
2019-11-16 12:16:07 -0800

    /books

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
-print(__name__)
+
+books = [
+    {
+        'name': 'Green Eggs and Ham',
+        'price': 7.99,
+        'isbn': 9780394800165
+    },
+    {
+        'name': 'The Cat In The Hat',
+        'price': 6.99,
+        'isbn': 9782371000193
+    }
+]
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+# GET /store
+@app.route('/books')
+def get_books():
+    return jsonify({'books': books})
 
 
 app.run(port=5000)
